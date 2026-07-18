514b521c2128196976c5c9f5d3c2c734f96d4e34
Saksham Verma <sakshamverma@Sakshams-MacBook-Air.local>
2026-07-19 01:30:51 +0530

    Fix working section image path

--- src/LandingPage/Working.tsx
+++ src/LandingPage/Working.tsx
@@ -12,8 +12,8 @@ const Working=()=>{
    <div className="flex px-16 items-center justify-between">
 
      <div className="relative  border border-red-500">
-        <img className="w-[30rem]" src="/Working/Girl.png" alt="girl" />
-        <div className="w-36 flex-col justify-center items-center gap-1 border border-bright-sun-400 rounded-xl py-3 px-1 backdrop-blur-md top-[15%] absolute right-0">
+        <img className="w-[30rem] h-auto object-contain" src="/Working/Girl.png" alt="girl" />
+        <div className="w-36 flex flex-col justify-center items-center gap-1 border border-bright-sun-400 rounded-xl py-3 px-1 backdrop-blur-md top-[15%] absolute right-0">
             <Avatar className="!h-16 !w-16" src="avatar-8.png" alt="girl" />
             <div className="text-sm font-semibold text-mine-shaft-200">
                 Complete your profile
