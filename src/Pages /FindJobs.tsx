ac55d0907c01e5be0286268c4fbeca489176a6e7
Saksham Verma <sakshamverma@Sakshams-MacBook-Air.local>
2026-07-22 16:28:42 +0530

    Remove unused state from FindJobs page

--- src/Pages /FindJobs.tsx
+++ src/Pages /FindJobs.tsx
@@ -1,6 +1,5 @@
 import { Divider } from "@mantine/core";
 import SearchBar from "../FindJobs/SearchBar";
-import { useState } from "react";
 import Jobs from "../FindJobs/Jobs";
    
 
@@ -8,7 +7,7 @@ import Jobs from "../FindJobs/Jobs";
 const FindJobs = ()=>
     
 {
-const [value, setValue] = useState<[number, number]>([20, 80]);
+
 return <div className="min-h-[100vh] bg-mine-shaft-950 font-['poppins']">
      <Divider size="xs" mx="md" />
      <SearchBar />
